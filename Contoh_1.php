99f197340ea3fa72193347e25343a9ea668618cc
RIKY <90605319+rikyfaturozi@users.noreply.github.com>
2021-09-18 01:42:19 +0700

    Add files via upload

---
+++ Contoh_1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh_1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "Hallo Saya Riky Faturozi";
+        echo "Saya hoby Berolahraga";
+        echo "Saya seorang mahasiswa UBSI PEMUDA";
+    }
+}
