e8e67663194ab894e5de2c3b5c1224be20701b6e
RIKY <90605319+rikyfaturozi@users.noreply.github.com>
2021-09-20 17:15:50 +0700

    Update Contoh_1.php

--- Contoh_1.php
+++ Contoh_1.php
@@ -3,8 +3,10 @@ class Contoh_1 extends CI_Controller
 {
     public function index ()
     {
-        echo "<b> Hallo Saya Riky Faturozi <br>";
-        echo "Saya hoby Berolahraga <br>";
-        echo "Saya seorang mahasiswa UBSI PEMUDA";
+        echo "<h1> Perkenalkan <br></h1>";
+        echo " Hallo, Saya Riky Faturozi <br>" ;   
+        echo "Saya Hoby Berolahraga <br>";
+        echo "Saya Seorang Mahasiswa UBSI PEMUDA <br>";
+        echo "Saya Umur 20 Tahun";
     }
 }
