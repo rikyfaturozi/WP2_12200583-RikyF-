47730ddb47ddb9c16bf3814a062ebc3f953a2d9d
RIKY <90605319+rikyfaturozi@users.noreply.github.com>
2021-09-27 14:23:43 +0700

    Modify file
    Contoh,1,2 & 3

--- Latihan1.php
+++ Latihan1.php
@@ -8,8 +8,10 @@ class Latihan1 extends CI_Controller
     public function penjumlah($n1, $n2)
     {
         $this->load->Model('Model_latihan1');
-        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
-        echo"<h1>Hasilnya <br></h1>". "<b>$n1" ." + ". "<b>$n2" ." <b>= "
-        .$hasil;
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+        
+        $this->load->view('View-latihan1', $data);
     }
 }
