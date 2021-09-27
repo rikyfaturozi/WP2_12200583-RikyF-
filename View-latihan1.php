38fd0ffcf854152364534e5d1708d25f58afaf7e
RIKY <90605319+rikyfaturozi@users.noreply.github.com>
2021-09-27 11:29:18 +0700

    Push via VCS
    contoh 3

---
+++ View-latihan1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan1</title>
+</head>
+<body>
+	 <h1>Hallo kawan.. yuk kita belajar web programming <br></h1>
+	 <b>Nilai1= <?= $nilai1; ?><br>
+        Nilai2= <?= $nilai2; ?>
+    <br>ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>		
+
+</body>
+</html>
