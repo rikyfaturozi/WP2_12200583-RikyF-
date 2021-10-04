aaa8b6d44496fcbc1dd57ec4ade035039a5bf3eb
RIKY <90605319+rikyfaturozi@users.noreply.github.com>
2021-10-04 18:01:24 +0700

    Push header VSC

---
+++ v-header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<title>WEB PROG 2|| Mantabs</title>
+    <link rel = "stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/style.css">
+</head>
+<body>
+    <div id ="og">
+        <header>
+            <hgroup>
+                <h1> RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana CodeIgniter</h3>
+           </hgroup>
+           <nav>
+               <ul>
+                   <li><a href =" <?php echo base_url().'index.php/WebLatihan' ?>">Home</a></li>
+                   <li><a href =" <?php echo base_url().'index.php/WebLatihan/about' ?>">About</a></li>
+            </nav>
+            <div class="clear"></div>
+        </header>    
